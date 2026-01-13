b33826f0f97028e8cc351338340950acc3312112
rngnnn <rngnnnclk@gmail.com>
2026-01-13 15:36:08 +0100

    add band name generator script

---
+++ task.py
@@ -0,0 +1,5 @@
+# name="welcome to the band name generator"
+print("welcome to the band name generator")
+name1=input("What's the name of the city you grew up in? \n")
+name2=input("What's your pet's name? \n")
+print("your band name could be " + name1 +" " + name2)
